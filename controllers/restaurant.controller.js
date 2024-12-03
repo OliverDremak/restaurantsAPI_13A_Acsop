8fed088054cf44e6ec73b10797beaecde8e794bf
Oliver Dremak <dremakoliver@gmail.com>
2024-12-03 08:18:11 +0100

    .

--- controllers/restaurant.controller.js
+++ controllers/restaurant.controller.js
@@ -1,5 +1,5 @@
 const restaurantModel = require('../models/etteremModel')
+
 exports.createRestaurant = (req, res, next) => {
     restaurantModel.create(req.body)
-    res.status(201)   
 }
--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,9 @@ const Model = require('../models/etteremModel')
 const restaurantCreate = require('../controllers/restaurant.controller')
 
 router.post('/', restaurantCreate)
+router.get('/', getAllRestaurants)
+router.get('/:id', getRestaurant)
+router.put('/:id', modifyRestaurant)
+router.delete('/:id', deleteRestaurant)
 
 module.exports = router;
